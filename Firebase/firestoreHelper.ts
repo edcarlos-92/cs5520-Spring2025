d9aeee27d0b456a3a2909abb00f2c6659b8321bf
edcarlos-92 <edcartech@gmail.com>
2025-03-31 15:29:55 -0700

    Enhance writeToDB function to support document updates with optional ID parameter

--- Firebase/firestoreHelper.ts
+++ Firebase/firestoreHelper.ts
@@ -10,9 +10,21 @@ import {
 import { database } from './firebaseSetup';
 import { GoalData, User } from '@/types';
 
-export async function writeToDB(data: GoalData | User, collectionName: string) {
+export async function writeToDB(
+  data: GoalData | User,
+  collectionName: string,
+  id?: string
+) {
   try {
-    const docRef = await addDoc(collection(database, collectionName), data);
+    if (id) {
+      const newDocRef = await setDoc(doc(database, collectionName, id), data, {
+        merge: true,
+      });
+    } else {
+      const docRef = await addDoc(collection(database, collectionName), data);
+    }
+
+    // const docRef = await addDoc(collection(database, collectionName), data);
   } catch (e) {
     console.error('Error adding document: ', e);
   }
@@ -49,6 +61,8 @@ export async function readDocFromDB(id: string, collectionName: string) {
     }
     return null;
   } catch (e) {
+    console.log({ id });
+    console.log({ collectionName });
     console.error('Error reading document: ', e);
   }
 }
